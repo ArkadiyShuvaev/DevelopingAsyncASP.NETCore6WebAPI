53e07a595574823faa595eb29a0d64de3af3eba9
Arkadiy Shuvaev <shuvaev.arkady@gmail.com>
2023-12-06 06:23:40 +0000

    added migration

---
+++ Migrations/20231206062258_AddIdentity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AsyncASP.NETCore6WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIdentity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
